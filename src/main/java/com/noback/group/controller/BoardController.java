a0e7e3dac38a16bf6e10359b5f181f4358226c1e
Laheartha <naverdg@gmail.com>
2022-05-18 10:33:13 +0900

    게시판 작업중

---
+++ src/main/java/com/noback/group/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.noback.group.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "board")
+public class BoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "boardList", method = RequestMethod.GET)
+	public String boardList(HttpSession session) {
+		logger.info("게시판 접속");
+		return "board/boardList";
+	}
+	
+}
